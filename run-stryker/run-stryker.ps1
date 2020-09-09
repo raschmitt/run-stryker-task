2c18861281d00431e4be9efce14e18f20008bdd2
Rogerio Schmitt <rogerio.schmitt@hbsis.com.br>
2020-09-09 19:07:16 -0300

    Update azure varibales

--- run-stryker/run-stryker.ps1
+++ run-stryker/run-stryker.ps1
@@ -1,15 +1,15 @@
 [CmdletBinding()]
 param(
-[Parameter(Mandatory=$False)]
-[string] $parameters,
-[Parameter(Mandatory=$True)]
-[string] $testProject
+    [Parameter(Mandatory = $False)]
+    [string] $parameters,
+    [Parameter(Mandatory = $True)]
+    [string] $testProject
 )
 
-install dotnet-stryker --tool-path $(Agent.BuildDirectory)/tools
+install dotnet-stryker --tool-path $env:Agent_BuildDirectory/tools
 
 $testPath = Get-ChildItem -Filter $testProject -Recurse | Select-Object -Expand Directory -First 1
 
 Set-Location $testPath
 
-$(Agent.BuildDirectory)/tools/dotnet-stryker $parameters
+$env:Agent_BuildDirectory/tools/dotnet-stryker
